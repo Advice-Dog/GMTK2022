0499bb2d7d7c7ceed82c1600870cd08f062219d3
Chris <chrisporter0111@gmail.com>
2022-07-15 15:12:01 -0700

    Drawing cards from the deck

--- Assets/Cards/Deck.cs
+++ Assets/Cards/Deck.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Deck : MonoBehaviour
 {
+    public static int MAX_DRAW_COUNT = 5;
+
     List<Card> cards = new List<Card>();
+
+    List<Card> drawPile = new List<Card>();
+
+    List<Card> hand = new List<Card>();
+
+    List<Card> discardPile = new List<Card>();
 
     public Deck()
     {
         SetDeck();
+        SetDrawPile();
+
+        DrawCards();
     }
 
     // Adding some default cards to the deck
@@ -24,5 +36,39 @@ public class Deck : MonoBehaviour
         {
             Debug.Log("Card: " + cards[i]);
         }
+    }
+
+    void SetDrawPile()
+    {
+        // Copy the cards to the Draw pile
+        drawPile = new List<Card>(cards);
+    }
+
+    void DrawCards()
+    {
+        while (hand.Count < MAX_DRAW_COUNT)
+        {
+            DrawCard();
+        }
+
+        for (int i = 0; i < hand.Count; i++)
+        {
+            Debug.Log("Drawn Card: " + hand[i]);
+        }
+    }
+
+    void DrawCard()
+    {
+        if (drawPile.Count == 0)
+        {
+            // shuffle the discard pile back into the draw pile
+            drawPile = discardPile.OrderBy(a => Random.value).ToList();
+            discardPile.Clear();
+        }
+
+        // Take the first card, remove it from the draw pile, and add it to the hand
+        Card card = drawPile[0];
+        drawPile = drawPile.GetRange(1, drawPile.Count - 1);
+        hand.Add (card);
     }
 }
