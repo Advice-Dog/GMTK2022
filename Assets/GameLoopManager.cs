cb2f868382ba1a71b64f319bd1d09cbeb00d915b
Chris <chrisporter0111@gmail.com>
2022-07-15 20:03:06 -0700

    Sliding room into position

--- Assets/GameLoopManager.cs
+++ Assets/GameLoopManager.cs
@@ -8,9 +8,23 @@ public class GameLoopManager : MonoBehaviour
 
     public GameObject pawnPrefab;
 
+    public GameObject roomPrefab;
+
     private Deck deck;
 
     private bool isFirstUpdate = true;
+
+    private static int GAME_STATE_BLANK = -1;
+
+    private static int GAME_STATE_SLIDING_IN = 0;
+
+    private static int GAME_STATE_PLAYING = 1;
+
+    private static int GAME_STATE_SLIDING_OUT = 2;
+
+    private static float ROOM_SLIDE_SPEED = 3.0f;
+
+    private int gameState = GAME_STATE_BLANK;
 
     private int pawnsSpawned = 0;
 
@@ -31,77 +45,126 @@ public class GameLoopManager : MonoBehaviour
         {
             Debug.Log("First update, setting player's hand.");
             isFirstUpdate = false;
-            deck.SetHand();
+            SpawnHand();
+        }
 
-            GameObject plane = GameObject.Find("Hand Plane");
-            Vector3 position = plane.transform.position;
-
-            float cardWidth = 0.12f;
-            float cardSpacer = 0.02f;
-
-            position.x -= (cardWidth + cardSpacer) * 2f;
-
-            List<Card> hand = deck.GetHand();
-            for (int i = 0; i < hand.Count; i++)
-            {
-                GameObject obj =
-                    Instantiate(prefab,
-                    position,
-                    Quaternion.Euler(new Vector3(0, -270, 30)));
-
-                obj.name = hand[i].ToString();
-                obj.transform.parent = plane.transform;
-
-                position.x += cardWidth + cardSpacer;
-            }
+        if (gameState == GameLoopManager.GAME_STATE_BLANK)
+        {
+            gameState = GameLoopManager.GAME_STATE_SLIDING_IN;
+            SpawnRoom();
+        }
+        else if (gameState == GameLoopManager.GAME_STATE_SLIDING_IN)
+        {
+            MoveRoom();
         }
 
         //Check for mouse click
         if (Input.GetMouseButtonDown(0))
         {
-            RaycastHit raycastHit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out raycastHit, 100f))
+            OnMouseClick();
+        }
+    }
+
+    void SpawnHand()
+    {
+        deck.SetHand();
+
+        GameObject plane = GameObject.Find("Hand Plane");
+        Vector3 position = plane.transform.position;
+
+        float cardWidth = 0.12f;
+        float cardSpacer = 0.02f;
+
+        position.x -= (cardWidth + cardSpacer) * 2f;
+
+        List<Card> hand = deck.GetHand();
+        for (int i = 0; i < hand.Count; i++)
+        {
+            GameObject obj =
+                Instantiate(prefab,
+                position,
+                Quaternion.Euler(new Vector3(0, -270, 30)));
+
+            obj.name = hand[i].ToString();
+            obj.transform.parent = plane.transform;
+
+            position.x += cardWidth + cardSpacer;
+        }
+    }
+
+    void SpawnRoom()
+    {
+        // Starting point to spawn the room at
+        GameObject roomSpawner = GameObject.Find("Room Spawner");
+        GameObject obj =
+            Instantiate(roomPrefab,
+            roomSpawner.transform.position,
+            Quaternion.Euler(new Vector3(0, 0, 0)));
+        obj.name = "Room";
+    }
+
+    void MoveRoom()
+    {
+        GameObject room = GameObject.Find("Room");
+        GameObject roomCenter = GameObject.Find("Room Center");
+
+        if (room.transform.position == roomCenter.transform.position)
+        {
+            gameState = GameLoopManager.GAME_STATE_PLAYING;
+            return;
+        }
+
+        float movement = ROOM_SLIDE_SPEED * Time.deltaTime;
+
+        Transform transform = room.transform;
+        transform.position =
+            Vector3
+                .MoveTowards(transform.position,
+                roomCenter.transform.position,
+                movement);
+    }
+
+    void OnMouseClick()
+    {
+        RaycastHit raycastHit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out raycastHit, 100f))
+        {
+            if (raycastHit.transform != null)
             {
-                if (raycastHit.transform != null)
+                string name =
+                    raycastHit.transform.gameObject.transform.parent.name;
+
+                //Our custom method.
+                Debug
+                    .Log("Name: " + name + ", Tag: " + raycastHit.collider.tag);
+
+                GameObject target = GameObject.Find(name);
+
+                //CurrentClickedGameObject(raycastHit.transform.gameObject);
+                if (target.tag == "Card")
                 {
-                    string name =
-                        raycastHit.transform.gameObject.transform.parent.name;
+                    List<Card> hand = deck.GetHand();
 
-                    //Our custom method.
-                    Debug
-                        .Log("Name: " +
-                        name +
-                        ", Tag: " +
-                        raycastHit.collider.tag);
+                    int index = 0;
 
-                    GameObject target = GameObject.Find(name);
-
-                    //CurrentClickedGameObject(raycastHit.transform.gameObject);
-                    if (target.tag == "Card")
+                    // find which card they selected
+                    Card card = hand[index];
+                    if (card is SpellCard)
                     {
-                        List<Card> hand = deck.GetHand();
-
-                        int index = 0;
-
-                        // find which card they selected
-                        Card card = hand[index];
-                        if (card is SpellCard)
-                        {
-                            Debug.Log("Player is playing a spell card.");
-                            // todo: allow the player to select the target
-                        }
-                        else if (card is PawnCard)
-                        {
-                            Debug.Log("Player is playing a pawn card.");
-
-                            // todo: spawn a pawn on the board
-                            SpawnPawn();
-                        }
-                        deck.RemoveCard (index);
-
-                        Destroy (target);
+                        Debug.Log("Player is playing a spell card.");
+                        // todo: allow the player to select the target
                     }
+                    else if (card is PawnCard)
+                    {
+                        Debug.Log("Player is playing a pawn card.");
+
+                        // todo: spawn a pawn on the board
+                        SpawnPawn();
+                    }
+                    deck.RemoveCard (index);
+
+                    Destroy (target);
                 }
             }
         }
@@ -119,6 +182,8 @@ public class GameLoopManager : MonoBehaviour
             Instantiate(pawnPrefab,
             position,
             Quaternion.Euler(new Vector3(0, 0, 0)));
+
+        obj.transform.parent = HeroSpawns.transform;
 
         pawnsSpawned++;
     }
